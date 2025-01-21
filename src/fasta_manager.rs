aa3d5f4a6dac0392aab33564d213db78d36f107d
Allen Kim <119351344+akim135@users.noreply.github.com>
2025-01-21 11:18:36 -0500

    Added a case to account for zero entries.

--- src/fasta_manager.rs
+++ src/fasta_manager.rs
@@ -213,6 +213,10 @@ mod test {
 
     fn test_fasta_file(fasta_name : &str, s : usize) {
         let fasta = open_fasta(fasta_name).unwrap();
+
+        if fasta.num_entries() != 0 {
+            fasta.indexed_entry(0).print_entry();
+        }
         assert_eq!(fasta.num_entries(), s);
         assert_eq!(fasta.filename(), fasta_name);
     }
